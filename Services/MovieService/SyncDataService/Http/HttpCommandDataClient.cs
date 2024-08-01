49c25ec85b06b2df1d7cff9a62bec357299ca056
Vladlavv <vovsiienko@myseneca.ca>
2024-08-01 14:18:42 +0300

    Added Http Data Client to access Command

---
+++ Services/MovieService/SyncDataService/Http/HttpCommandDataClient.cs
@@ -0,0 +1,44 @@
+﻿using MovieService.Dtos.MovieDto;
+using System;
+using System.Text;
+using System.Text.Json;
+
+/// <summary>
+/// Summary description for Class1
+/// </summary>
+public class HttpCommandDataClient : IHttpCommandDataClient
+{
+	private readonly HttpClient _httpClient;
+	private readonly string _commandServiceURL;
+	public HttpCommandDataClient(HttpClient httpClient)
+	{
+		_httpClient = httpClient;
+	}
+
+	public async Task SendMovieToReview(MovieGetDto dto)
+	{
+		try
+		{
+			var httpContent = new StringContent
+			(
+				JsonSerializer.Serialize(dto),
+				Encoding.UTF8,
+				"application/json"
+			);
+
+			var response = await _httpClient.PostAsync(_commandServiceURL, httpContent);
+			if(response.IsSuccessStatusCode)
+			{
+				//ok
+			}
+			else
+			{
+				//not ok
+			}
+		}
+		catch (Exception ex)
+		{
+
+		}
+	}
+}
---
+++ Services/MovieService/SyncDataService/Http/IHttpCommandDataClient.cs
@@ -0,0 +1,10 @@
+﻿using MovieService.Dtos.MovieDto;
+using System;
+
+/// <summary>
+/// Summary description for Class1
+/// </summary>
+interface IHttpCommandDataClient
+{
+	Task SendMovieToReview(MovieGetDto dto);
+}
