0e366c41ec9699f00adfb2d93e0c9ff94ae632e0
Vladlavv <vovsiienko@myseneca.ca>
2024-09-23 19:17:02 +0300

    Added Request/Response logging and CORS in UserService

--- Services/UserService/Program.cs
+++ Services/UserService/Program.cs
@@ -1,3 +1,4 @@
+using Microsoft.AspNetCore.HttpLogging;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using MovieReviewApp.Common.Middlewares;
@@ -28,6 +29,34 @@ namespace ReviewService
 
 		private static void ConfigureServices(IServiceCollection services, IConfiguration configuration)
 		{
+            services.AddHttpLogging(logging =>
+            {
+                logging.LoggingFields = HttpLoggingFields.All;
+                logging.RequestHeaders.Add("sec-ch-ua");
+                logging.MediaTypeOptions.AddText("application/javascript");
+                logging.RequestBodyLogLimit = 4096;
+                logging.ResponseBodyLogLimit = 4096;
+                logging.CombineLogs = true;
+            });
+
+            services.AddCors(options =>
+            {
+                options.AddPolicy("AllowSpecificOrigin",
+                    builder =>
+                    {
+                        builder.WithOrigins("http://localhost:4200")
+                               .AllowAnyHeader()
+                               .AllowAnyMethod();
+                    });
+            });
+
+            services.AddControllers();
+
+			services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+
+			services.AddEndpointsApiExplorer();
+			services.AddSwaggerGen();
+
 			services.AddDbContext<ApplicationDbContext>(options =>
 			  options.UseSqlServer(configuration.GetConnectionString("DefaultConnection")));
 
@@ -35,42 +64,12 @@ namespace ReviewService
 				.AddEntityFrameworkStores<ApplicationDbContext>()
 				.AddDefaultTokenProviders();
 
-			services.Configure<IdentityOptions>(options =>
-			{
-				// Password settings
-				var passwordOptions = configuration.GetSection("PasswordRules");
-				options.Password.RequireDigit = passwordOptions.GetValue<bool>("RequireDigit");
-				options.Password.RequireLowercase = passwordOptions.GetValue<bool>("RequireLowercase");
-				options.Password.RequireUppercase = passwordOptions.GetValue<bool>("RequireUppercase");
-				options.Password.RequireNonAlphanumeric = passwordOptions.GetValue<bool>("RequireNonAlphanumeric");
-				options.Password.RequiredLength = passwordOptions.GetValue<int>("RequiredLength");
-				options.Password.RequiredUniqueChars = passwordOptions.GetValue<int>("RequiredUniqueChars");
+			ConfigureIdentity(services, configuration);
 
-				// Lockout settings
-				var lockoutOptions = configuration.GetSection("LockoutRules");
-				options.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(lockoutOptions.GetValue<int>("DefaultLockoutTimeSpanInMinutes"));
-				options.Lockout.MaxFailedAccessAttempts = lockoutOptions.GetValue<int>("MaxFailedAccessAttempts");
-				options.Lockout.AllowedForNewUsers = lockoutOptions.GetValue<bool>("AllowedForNewUsers");
-
-				// User settings
-				var userOptions = configuration.GetSection("UserRules");
-				options.User.AllowedUserNameCharacters = userOptions.GetValue<string>("AllowedUserNameCharacters");
-				options.User.RequireUniqueEmail = userOptions.GetValue<bool>("RequireUniqueEmail");
-			});
+			services.AddJwtAuthentication(configuration);
 
 			services.AddScoped<IUserService, UserService.Service.UserService>();
             services.AddScoped<IJwtGeneratorService, JwtGeneratorService>();
-
-
-
-            services.AddControllers();
-
-			services.AddEndpointsApiExplorer();
-			services.AddSwaggerGen();
-
-			services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
-
-			services.AddJwtAuthentication(configuration);
 
             services.AddExceptionHandler<UserGlobalExceptionHandler>();
             services.AddExceptionHandler<GlobalExceptionHandler>();
@@ -79,14 +78,17 @@ namespace ReviewService
 
 		private static void Configure(WebApplication app)
 		{
-			if (app.Environment.IsDevelopment())
+            app.UseHttpLogging();
+            if (app.Environment.IsDevelopment())
 			{
 				app.UseSwagger();
 				app.UseSwaggerUI();
 			}
             app.UseExceptionHandler();
 
-			app.UseHttpsRedirection();
+            app.UseCors("AllowSpecificOrigin");
+
+            app.UseHttpsRedirection();
 
 			app.UseAuthentication();
 			app.UseAuthorization();
@@ -95,5 +97,30 @@ namespace ReviewService
 
         }
 
+		private static void ConfigureIdentity(IServiceCollection services, IConfiguration configuration)
+		{
+            services.Configure<IdentityOptions>(options =>
+            {
+                // Password settings
+                var passwordOptions = configuration.GetSection("PasswordRules");
+                options.Password.RequireDigit = passwordOptions.GetValue<bool>("RequireDigit");
+                options.Password.RequireLowercase = passwordOptions.GetValue<bool>("RequireLowercase");
+                options.Password.RequireUppercase = passwordOptions.GetValue<bool>("RequireUppercase");
+                options.Password.RequireNonAlphanumeric = passwordOptions.GetValue<bool>("RequireNonAlphanumeric");
+                options.Password.RequiredLength = passwordOptions.GetValue<int>("RequiredLength");
+                options.Password.RequiredUniqueChars = passwordOptions.GetValue<int>("RequiredUniqueChars");
+
+                // Lockout settings
+                var lockoutOptions = configuration.GetSection("LockoutRules");
+                options.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(lockoutOptions.GetValue<int>("DefaultLockoutTimeSpanInMinutes"));
+                options.Lockout.MaxFailedAccessAttempts = lockoutOptions.GetValue<int>("MaxFailedAccessAttempts");
+                options.Lockout.AllowedForNewUsers = lockoutOptions.GetValue<bool>("AllowedForNewUsers");
+
+                // User settings
+                var userOptions = configuration.GetSection("UserRules");
+                options.User.AllowedUserNameCharacters = userOptions.GetValue<string>("AllowedUserNameCharacters");
+                options.User.RequireUniqueEmail = userOptions.GetValue<bool>("RequireUniqueEmail");
+            });
+        }
 	}
 }
